9df9bd66667e7fe76b8ac9c6e520b0843740256d
唐海龙 <1936202711@qq.com>
2023-04-10 01:41:38 +0800

    1.41

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => { 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('04-10-01:33 ! \n');
+    res.end('04-10-01:41 ! \n');
 }); 
 server.listen(port, hostname, () => { 
     console.log(`Server running at http://${hostname}:${port}/`);
